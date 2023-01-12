30401187543f43ddd954d998e0bd35e017a91df6
tahersn <taher.snoussi@esprit.tn>
2023-01-12 11:01:09 +0100

    add list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,59 @@
+import { useDraggable } from "react-use-draggable-scroll";
+import { useRef } from "react";
+
 const Home = () => {
-  const tab = [
-    { nom: "taher", age: 21, passion: "dev" },
-    { nom: "yessine", age: 21, passion: "web" },
-  ];
+  const ref = useRef(null);
+  const { events } = useDraggable(ref);
 
   return (
-    <div className="flex h-full items-center justify-center text-xl flex-col gap-5">
-      {tab.map((item) => {
-        return (
-          <h1 className="text-red-500 font-extrabold" key={item.nom}>
-            Passion:{item.passion}
-          </h1>
-        );
-      })}
+    <div className="flex h-full overflow-auto " ref={ref} {...events}>
+      <div className="flex h-80 gap-3 p-8 flex-row cursor-grab">
+        <div class="card w-96 bg-indigo-500 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div class="card w-96  bg-orange-600 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div class="card w-96 bg-indigo-500 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div class="card w-96  bg-orange-600 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div class="card w-96  bg-orange-600 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
